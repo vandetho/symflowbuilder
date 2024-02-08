d03345fa23d7593002285da72dfc49fb624b617b
Vandetho <thovandeth@gmail.com>
2024-02-08 16:08:35 +0700

    Refactor code and add functionality to delete transitions
    The code has been reorganized for better readability, and types have been defined for workflow transitions and places. The logic for adding places and transitions has been updated, including a new feature to delete specific workflow transitions. Content within the Card components has also been slightly modified for clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 
 import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
 import Select from '@/components/select';
 import { useFieldArray, useForm, useWatch } from 'react-hook-form';
 import TextField from '@/components/text-field';
 import Switch from '@/components/switch';
 import { Button } from '@/components/ui/button';
+
+type WorkflowPlace = {
+    name: string;
+};
+
+type WorkflowTransition = {
+    name: string;
+    from: string;
+    to: string;
+};
 
 type WorkflowConfig = {
     name: string;
@@ -16,13 +25,13 @@ type WorkflowConfig = {
         property: string;
     };
     type: string;
-    places: Array<{ value: string }>;
+    places: WorkflowPlace[];
     initialMarking: string;
-    transitions: string[];
+    transitions: WorkflowTransition[];
 };
 
 export default function Home() {
-    const { control, handleSubmit, getValues } = useForm<WorkflowConfig>({
+    const { control, handleSubmit } = useForm<WorkflowConfig>({
         defaultValues: {
             name: '',
             auditTrail: false,
@@ -39,24 +48,38 @@ export default function Home() {
         control,
         name: 'places',
     });
+    const {
+        fields: transitionFields,
+        append: appendTransition,
+        remove: removeTransition,
+    } = useFieldArray({
+        control,
+        name: 'transitions',
+    });
     const watchPlaces = useWatch({ name: 'places', control });
 
     const places = React.useMemo(() => {
-        return watchPlaces
-            .filter((field) => !!field.value)
-            .map((field) => ({ label: field.value, value: field.value }));
+        return watchPlaces.filter((field) => !!field.name).map((field) => ({ label: field.name, value: field.name }));
     }, [watchPlaces]);
 
     const onAddPlace = React.useCallback(() => {
-        append({ value: '' });
-    }, []);
+        append({ name: '' });
+    }, [append]);
+
+    const onAddTransition = React.useCallback(() => {
+        appendTransition({ name: '', to: '', from: '' });
+    }, [appendTransition]);
+
+    const onSubmit = (data: WorkflowConfig) => {
+        console.log(data);
+    };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="flex items-center gap-4">
                 <Card className="w-[350px]">
                     <CardHeader>
-                        <CardTitle>Create new workflow config</CardTitle>
+                        <CardTitle>Create new workflow configuration</CardTitle>
                         <CardDescription>The best way to build and visualize workflow for symfony</CardDescription>
                     </CardHeader>
                     <CardContent>
@@ -85,7 +108,9 @@ export default function Home() {
                             </div>
                             <div className="flex flex-col gap-3">
                                 <p className="text-lg">Places</p>
-                                <Button onClick={onAddPlace}>Add Place</Button>
+                                <Button variant="secondary" onClick={onAddPlace}>
+                                    Add Place
+                                </Button>
                                 {fields.map((field, index) => (
                                     <div className={'flex gap-2'} key={field.id}>
                                         <TextField
@@ -111,28 +136,45 @@ export default function Home() {
                             />
                             <div className="flex flex-col gap-3">
                                 <p className="text-lg">Transitions</p>
-                                <Input type="text" placeholder="transition" />
-                                <Select
-                                    control={control}
-                                    name={`transition.from`}
-                                    className="w-[300px]"
-                                    placeholder="From"
-                                    items={places}
-                                />
-                                <Select
-                                    control={control}
-                                    name={`transition.to`}
-                                    className="w-[300px]"
-                                    placeholder="To"
-                                    items={places}
-                                />
+                                <Button variant="secondary" onClick={onAddTransition}>
+                                    Add Transition
+                                </Button>
+                                {transitionFields.map((field, index) => (
+                                    <div className={'flex flex-col gap-2'} key={field.id}>
+                                        <TextField
+                                            control={control}
+                                            name={`transitions.${index}.name`}
+                                            type="text"
+                                            placeholder="Transition name"
+                                            key={field.id}
+                                        />
+                                        <Select
+                                            control={control}
+                                            name={`transitions.${index}.from`}
+                                            className="w-[300px]"
+                                            placeholder="From"
+                                            items={places}
+                                        />
+                                        <Select
+                                            control={control}
+                                            name={`transitions.${index}.to`}
+                                            className="w-[300px]"
+                                            placeholder="To"
+                                            items={places}
+                                        />
+                                        <Button variant="destructive" onClick={() => removeTransition(index)}>
+                                            Remove
+                                        </Button>
+                                    </div>
+                                ))}
+                                <Button onClick={handleSubmit(onSubmit)}>Save</Button>
                             </div>
                         </div>
                     </CardContent>
                 </Card>
                 <Card className="w-[350px]">
                     <CardHeader>
-                        <CardTitle>Create new workflow config</CardTitle>
+                        <CardTitle>View your workflow configuration</CardTitle>
                         <CardDescription>The best way to build and visualize workflow for symfony</CardDescription>
                     </CardHeader>
                     <CardContent></CardContent>
