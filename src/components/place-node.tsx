225fcb6e80b3d298f9a9d7fbbcd4764ab71ec05f
Vandetho <thovandeth@gmail.com>
2024-02-16 17:15:19 +0700

    Incorporate NodeToolbar in place and transition nodes
    Updated 'place-node.tsx' and 'transition-node.tsx' to include NodeToolbar functionality. The UI of each node is now wrapped in a card format using the 'Card' and 'CardContent' elements, and edited through an 'Input' element embedded in the NodeToolbar.

--- src/components/place-node.tsx
+++ src/components/place-node.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Handle, NodeProps, Position } from 'reactflow';
+import { Handle, NodeProps, NodeToolbar, Position } from 'reactflow';
 import { WorkflowPlace } from '@/types/WorkflowPlace';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
 
 interface PlaceNodeProps extends NodeProps {
     data: WorkflowPlace;
@@ -8,13 +10,26 @@ interface PlaceNodeProps extends NodeProps {
 
 const PlaceNode = React.memo<PlaceNodeProps>(({ data }) => {
     return (
-        <div className="px-4 py-2 shadow-md rounded-lg border-2 border-primary">
-            <div className="flex">
-                <div className="text-lg font-bold">{data.name}</div>
+        <React.Fragment>
+            <NodeToolbar position={Position.Top}>
+                <Card className="w-[350px]">
+                    <CardContent>
+                        <Input type="text" value={data.name} />
+                    </CardContent>
+                </Card>
+            </NodeToolbar>
+            <div className="px-4 py-2 shadow-md rounded-lg border-2 border-primary">
+                <div className="flex">
+                    <div className="text-lg font-bold">{data.name}</div>
+                </div>
+                <Handle type="target" position={Position.Left} className="w-1 h-8 border-none rounded-sm !bg-primary" />
+                <Handle
+                    type="source"
+                    position={Position.Right}
+                    className="w-1 h-8 border-none rounded-sm !bg-primary"
+                />
             </div>
-            <Handle type="target" position={Position.Left} className="w-1 h-8 border-none rounded-sm !bg-primary" />
-            <Handle type="source" position={Position.Right} className="w-1 h-8 border-none rounded-sm !bg-primary" />
-        </div>
+        </React.Fragment>
     );
 });
 
--- src/components/transition-node.tsx
+++ src/components/transition-node.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Handle, NodeProps, Position } from 'reactflow';
+import { Handle, NodeProps, NodeToolbar, Position } from 'reactflow';
 import { WorkflowTransition } from '@/types/WorkflowTransition';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
 
 interface TransitionNodeProps extends NodeProps {
     data: WorkflowTransition;
@@ -8,13 +10,26 @@ interface TransitionNodeProps extends NodeProps {
 
 const TransitionNode = React.memo<TransitionNodeProps>(({ data }) => {
     return (
-        <div className="px-4 py-2 shadow-md rounded-full border-2 border-teal-500">
-            <div className="flex">
-                <div className="text-lg font-bold">{data.name}</div>
+        <React.Fragment>
+            <NodeToolbar position={Position.Top}>
+                <Card className="w-[350px]">
+                    <CardContent>
+                        <Input type="text" value={data.name} />
+                    </CardContent>
+                </Card>
+            </NodeToolbar>
+            <div className="px-4 py-2 shadow-md rounded-full border-2 border-teal-500">
+                <div className="flex">
+                    <div className="text-lg font-bold">{data.name}</div>
+                </div>
+                <Handle type="target" position={Position.Left} className="w-1 h-4 border-none rounded-sm bg-teal-500" />
+                <Handle
+                    type="source"
+                    position={Position.Right}
+                    className="w-1 h-4 border-none rounded-sm bg-teal-500"
+                />
             </div>
-            <Handle type="target" position={Position.Left} className="w-1 h-4 border-none rounded-sm bg-teal-500" />
-            <Handle type="source" position={Position.Right} className="w-1 h-4 border-none rounded-sm bg-teal-500" />
-        </div>
+        </React.Fragment>
     );
 });
 
