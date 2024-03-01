244a00f33007d81e28d31bab337e8a7af8438880
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2024-03-01 15:44:58 +0700

    Remove authentication and related functionalities
    This commit removes the functionalities related to user authentication and connection to MongoDB. Files responsible for handling sign in, sign up, refresh token, MongoDB connection, and related routes were removed as part of the cleanup. Also, dependencies such as "bcrypt", "mongodb", and "next-auth" were dropped from package.json.

--- src/app/api/sign-in/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export function POST(req: NextRequest) {
-    const body = req.json();
-    return NextResponse.json({ message: 'Hello, world!' });
-}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+import React, { useState } from 'react';
+import { supabase } from '@/lib/initSupabase';
+
+const LoginPage = () => {
+    const [emailAddress, setEmailAddress] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isTokenSent, setIsTokenSent] = useState<boolean>(false);
+
+    const authenticateUser = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (!emailAddress) {
+            alert('Email address is required');
+            return;
+        }
+
+        setIsLoading(true);
+        const { data, error } = await supabase.auth.signInWithOtp({
+            email: emailAddress,
+            options: {
+                emailRedirectTo: window.location.origin,
+            },
+        });
+
+        if (error) {
+            console.error(error);
+            throw new Error('Something went wrong');
+        }
+
+        if (data) {
+            setIsTokenSent(true);
+            setTimeout(() => {
+                setIsTokenSent(false); // show the login ui after 5 seconds
+                setIsLoading(false);
+            }, 5000);
+        }
+    };
+
+    return (
+        <form
+            onSubmit={authenticateUser}
+            className="flex flex-col gap-3 justify-center items-center h-screen max-w-lg mx-auto"
+        >
+            <label>Login with magic link 🧙🏽‍♂️</label>
+            <input
+                type="text"
+                placeholder="Enter email address"
+                className="border border-slate-200 w-full px-3 py-2 rounded-lg"
+                onChange={(e) => setEmailAddress(e.target.value)}
+                value={emailAddress}
+            />
+
+            <button
+                type="submit"
+                className="px-3 py-2 bg-slate-900 text-white rounded-lg text-base w-full"
+                disabled={isTokenSent || isLoading}
+            >
+                {isTokenSent
+                    ? 'Token sent...please check your email address'
+                    : isLoading
+                      ? 'One moment please...'
+                      : 'Send magic link'}
+            </button>
+        </form>
+    );
+};
+
+export default LoginPage;
