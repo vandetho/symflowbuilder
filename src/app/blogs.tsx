86d9ddb8eeef912e9a8ee82811edd948e0c2f6f1
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2025-02-20 11:40:17 +0700

    Update blog links and add hydration warning suppression
    Updated incorrect blog links to correct usernames for consistency and added a new blog entry. Also introduced `suppressHydrationWarning` in the layout for improved hydration handling during SSR.

--- src/app/blogs.tsx
+++ src/app/blogs.tsx
@@ -36,31 +36,37 @@ const blogs = [
         title: 'Understanding and Implementing the Workflow Component in Symfony',
         description:
             'The Workflow component in Symfony, focusing on managing object life-cycles within applications. It covers the basics of setting up and implementing the component, distinguishing between "Workflow" for complex processes and "State Machine" for linear processes.',
-        href: 'https://medium.com/@thovandeth/understanding-and-implementing-the-workflow-component-in-symfony-8b1764efa057',
+        href: 'https://medium.com/@vandetho/understanding-and-implementing-the-workflow-component-in-symfony-8b1764efa057',
+    },
+    {
+        title: 'Implementing Workflow Component In Symfony With Multiple States Simultaneously',
+        description:
+            "This advanced tutorial dives into configuring and utilizing the workflow type 'workflow' as opposed to 'state_machine', showcasing a more dynamic approach suitable for non-linear processes.",
+        href: 'https://medium.com/@vandetho/implementing-workflow-component-in-symfony-with-multiple-states-simultaneously-e553b82debbe',
     },
     {
         title: 'Implementing and Managing Events in Symfony Workflows',
         description:
             'A comprehensive guide on implementing and managing events within Symfony workflows. It serves as a sequel to an introductory article on the Symfony Workflow component, focusing on advanced workflow management through event handling.',
-        href: 'https://medium.com/@thovandeth/implementing-and-managing-events-in-symfony-workflows-f5f8b9372cc6',
+        href: 'https://medium.com/@vandetho/implementing-and-managing-events-in-symfony-workflows-f5f8b9372cc6',
     },
     {
-        title: 'Implementing Workflow Component In Symfony With Multiple States Simultaneously',
+        title: 'Simplifying Event Handling in Symfony Workflows with the Event Attribute (Symfony 7.1)',
         description:
-            " This advanced tutorial dives into configuring and utilizing the workflow type 'workflow' as opposed to 'state_machine', showcasing a more dynamic approach suitable for non-linear processes.",
-        href: 'https://medium.com/@thovandeth/implementing-workflow-component-in-symfony-with-multiple-states-simultaneously-e553b82debbe',
+            "A detailed guide on simplifying event handling in Symfony workflows with the 'event' attribute, introduced in Symfony 7.1. It covers the definition of events, the configuration of workflows, and practical examples of event handling within Symfony applications.",
+        href: 'https://medium.com/@vandetho/simplifying-event-handling-in-symfony-workflows-with-the-event-attribute-symfony-7-1-052a3ded0f28',
     },
     {
         title: 'Understanding and Implementing the Symfony Workflow Component in Laravel.',
         description:
             'A detailed guide on integrating the Symfony Workflow component into Laravel applications, allowing for sophisticated workflow or state machine management. It covers the installation of the component, configuration of workflows including states and transitions, and practical implementation examples within a Laravel model.',
-        href: 'https://medium.com/@thovandeth/understanding-and-implementing-the-symfony-workflow-component-in-laravel-df5afbdff910',
+        href: 'https://medium.com/@vandetho/understanding-and-implementing-the-symfony-workflow-component-in-laravel-df5afbdff910',
     },
     {
         title: 'Creating a Custom Hydrator in Doctrine and Symfony for Enhanced Performance with DTOs',
         description:
             'The process of enhancing performance in Symfony applications by creating a custom hydrator in Doctrine for the efficient use of Data Transfer Objects (DTOs). It covers the definition of DTOs, the rationale behind using custom hydrators for DTO generation instead of loading complete entities, and provides a detailed step-by-step guide for implementation.',
-        href: 'https://medium.com/@thovandeth/creating-a-custom-hydrator-in-doctrine-and-symfony-for-enhanced-performance-with-dtos-f9f4fce8f035',
+        href: 'https://medium.com/@vandetho/creating-a-custom-hydrator-in-doctrine-and-symfony-for-enhanced-performance-with-dtos-f9f4fce8f035',
     },
 ];
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
                 <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
                     <NavBar />
