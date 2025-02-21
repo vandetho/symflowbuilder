980149ae9c91c6f5168b6732592d205a5e347e37
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2025-02-21 13:36:55 +0700

    Add separator component and overhaul multi-select UI
    Introduced a reusable `Separator` component and integrated it into the updated `MultiSelect` component. Enhanced the multi-select with new features including animation, improved styling, and better accessibility, while also making it more customizable. Updated dependencies to include `@radix-ui/react-separator`.

---
+++ src/components/multi-select-field.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { MultiSelect, Option } from '@/components/ui/multi-select';
+import { Control } from 'react-hook-form';
+
+interface MultiSelectFieldProps {
+    control: Control<any>;
+    name: string;
+    label?: string;
+    description?: string;
+    options: Option[];
+    disabled?: boolean;
+    required?: boolean;
+    className?: string;
+}
+
+export const MultiSelectField: React.FC<MultiSelectFieldProps> = ({
+    control,
+    name,
+    label,
+    description,
+    options,
+    disabled,
+    required,
+    className,
+}) => {
+    const localId = React.useId();
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    {label && (
+                        <FormLabel htmlFor={localId} aria-required={required}>
+                            {label}
+                            {required && <span className="ml-1 text-red-500">*</span>}
+                        </FormLabel>
+                    )}
+                    <MultiSelect
+                        id={localId}
+                        {...field}
+                        className={className}
+                        disabled={disabled}
+                        options={options}
+                        onValueChange={field.onChange}
+                    />
+                    <FormMessage />
+                    {description && <FormDescription>{description}</FormDescription>}
+                </FormItem>
+            )}
+        />
+    );
+};
