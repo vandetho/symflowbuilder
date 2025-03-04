fe89f2fe216ea4902e94b9c91d6395c23b7da9cb
Vandeth THO <1325087+vandetho@users.noreply.github.com>
2025-03-04 21:37:28 +0700

    Use dynamic import for ReactJson to disable SSR
    Converted the import of `react-json-view` to a dynamic import with SSR disabled. This prevents server-side rendering issues and ensures compatibility with environments where `react-json-view` relies on browser-specific features.

--- src/components/react-json-viewer.tsx
+++ src/components/react-json-viewer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactJson from 'react-json-view';
 import { WorkflowConfigYaml } from '@/types/WorkflowConfigYaml';
+import dynamic from 'next/dynamic';
+
+const DynamicReactJson = dynamic(() => import('react-json-view'), { ssr: false });
 
 interface ReactJsonViewerProps {
     yamlConfig: WorkflowConfigYaml | undefined;
 }
 
 export const ReactJsonViewer = React.memo<ReactJsonViewerProps>(({ yamlConfig }) => {
-    return <div className="flex flex-col gap-3">{yamlConfig && <ReactJson theme="monokai" src={yamlConfig} />}</div>;
+    return (
+        <div className="flex flex-col gap-3">{yamlConfig && <DynamicReactJson theme="monokai" src={yamlConfig} />}</div>
+    );
 });
 
 ReactJsonViewer.displayName = 'JsonMarkdown';
